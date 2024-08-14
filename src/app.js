01505607c0f82404b14e03b529688c219d0a2c85
Ivan Perez <pumi0751@gmail.com>
2024-08-14 17:01:39 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["cat", "My Grandma", "My Dad", "bird", "tiger"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when i was sleeping",
+  "while I was exercising",
+  "during my luch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * b - a) + a;
+}
+
+function getRandom(anyArray) {
+  let max = who.length;
+  let min = 0;
+  console.log(max, min, anyArray);
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(who);
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
